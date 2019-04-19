6ab2e2307886b0e398d7ea533d9fa30692448301
Eric Zhang <ericz3@illinois.edu>
2019-04-18 20:11:55 -0500

    added axis, adjusted translations, changed colors

--- visualization.js
+++ visualization.js
@@ -13,7 +13,11 @@ $(function() {
 function visualize(data) {
   // Boilerplate:
   const canvasDimension = { width: 1024, height: 800 };
-  const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+  const margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50 };
 
   const effectiveDimension = {
     width: canvasDimension.width - margin.left - margin.right,
@@ -37,7 +41,7 @@ function visualize(data) {
     .attr("height", canvasDimension.height)
     .attr("x", 0)
     .attr("y", 0)
-    .attr("fill", "dodgerblue")
+    .attr("fill", "LightBlue")
     .attr("stroke", "black");
 
   var background = svg
@@ -46,10 +50,11 @@ function visualize(data) {
     .attr("height", effectiveDimension.height)
     .attr("x", margin.left)
     .attr("y", margin.top)
-    .attr("fill", "grey")
-    .attr("stroke", "blue");
+    .attr("fill", "lightgrey")
+    .attr("stroke", "LightSkyBlue");
 
   const points = [[1, 1], [500, 35], [300, 400], [100, 200], [260, 50]];
+  const axisPoints = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]];
   const categories = [
     ["calories", 160],
     ["protein", 6],
@@ -63,15 +68,17 @@ function visualize(data) {
   console.log("Categories: " + categories);
   let theta = 360 / categories.length;
   const angles = [];
-  for (let a = 1; a <= 3; a += 1) {
+  const firstGraphX = canvasDimension.width/5;
+  const graphY = canvasDimension.height/3;
+  for (let a = 0; a < 3; a += 1) {
     // Spacing the polygons horizontally
-    let translation = a * 250;
+    var translation = firstGraphX + a * (canvasDimension.width - 2 * firstGraphX)/2;
 
     // The cereal we are drawing the polygon for
     const cerealRow = data[14 + a];
 
     // Radius of the polygon
-    let scale = 250;
+    let scale = 125;
 
     categories.forEach((category, index) => {
       percentages[index] = cerealRow[category[0]] / category[1];
@@ -81,14 +88,21 @@ function visualize(data) {
     for (let i = 0; i < categories.length; i += 1) {
       angles[i] = theta * i;
       let magnitude = scale * percentages[i];
+      axisPoints[i] =  [
+        scale * Math.cos(degreesToRadians(angles[i] + 90)) + translation,
+        scale * Math.sin(degreesToRadians(angles[i] + 90)) + graphY
+      ];
       points[i] = [
         magnitude * Math.cos(degreesToRadians(angles[i] + 90)) + translation,
-        magnitude * Math.sin(degreesToRadians(angles[i] + 90)) + 400
+        magnitude * Math.sin(degreesToRadians(angles[i] + 90)) + graphY
       ];
     }
     console.log("Angles: " + angles);
     console.log("Points: " + points);
+    console.log("Axis Points: " + axisPoints);
     drawPolygon(svg, points);
+    drawAxis(svg, axisPoints, translation, graphY);
+    drawBox(svg, translation, graphY + scale * 1.25);
   }
 }
 
@@ -115,7 +129,31 @@ function drawPolygon(svg, points) {
   svg
     .append("polygon")
     .attr("points", pointsString)
-    .style("fill", "green")
+    .style("fill", "LimeGreen")
     .style("stroke", "black")
     .style("strokeWidth", "10px");
+}
+
+function drawAxis(svg, points, originX, originY) {
+  for(let i = 0; i < points.length; i+=1){
+    svg.append("line")
+      .style("stroke", "black")
+      .attr("x1", originX)
+      .attr("y1", originY)
+      .attr("x2", points[i][0])
+      .attr("y2", points[i][1]);
+  }
+}
+
+function drawBox(svg, x, y){
+  var boxWidth = 250;
+  var boxHeight = 300;
+  svg.append("rect")
+    .attr("rx", 6)
+    .attr("ry", 6)
+    .attr("x", x-boxWidth/2)
+    .attr("y", y)
+    .attr("width", boxWidth)
+    .attr("height", boxHeight)
+    .style("fill", "CadetBlue");
 }
